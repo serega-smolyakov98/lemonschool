46418927d564614c2f19186b36beb0b0f61504a8
serega-smolyakov98 <smolyakov-serega@mail.ru>
2017-04-24 14:55:21 +0300

    commit

--- Mogo/scripts/script.js
+++ Mogo/scripts/script.js
@@ -190,3 +190,33 @@ $(document).ready(function(){
       })
     });
 // SLIDER-2
+
+// MAP
+$(document).ready(function(){
+    $(".hider").click(function(){
+        $("#hidden").slideToggle("slow");
+        return false;
+    });
+});
+// MAP
+
+// FIDE 
+$(window).scroll(function () {
+        $(' .mov').each(function () {
+            var imagePos = $(this).offset().top;
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow+600) {
+                $(this).addClass('fadeInLeft');
+            }
+        });
+    });﻿
+$(window).scroll(function () {
+    $(' .mov2').each(function () {
+        var imagePos = $(this).offset().top;
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow+700) {
+            $(this).addClass('fadeInRight');
+        }
+    });
+});﻿
+// FIDE
